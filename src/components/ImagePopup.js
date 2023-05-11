f9f28517ea1fe87e207e6885e621d110f8940345
Эльнар Мустафин <mustafinelnare@gmail.com>
2023-05-11 20:05:15 +0400

    fix: imagepopup

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,23 +1,23 @@
-export default function ImagePopup(card) {
+export default function ImagePopup(props) {
   return (
     <div
       className={`popup popup_open-image ${
-        card.card.name ? "popup_opened" : ""
+        props.card.name ? "popup_opened" : ""
       }`}
     >
       <div className="popup__container popup__container_image">
         <button
           className="popup__close-button"
           type="button"
-          onClick={card.onClose}
+          onClick={props.onClose}
         ></button>
         <div className="popup__place-image">
           <img
             className="popup__image"
-            src={card.card.link}
-            alt={card.card.name}
+            src={props.card.link}
+            alt={props.card.name}
           />
-          <p className="popup__subtitle">{card.card.name}</p>
+          <p className="popup__subtitle">{props.card.name}</p>
         </div>
       </div>
     </div>
